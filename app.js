5c97d2ad6604b060830afe25292443e4abf28fce
sulaiman-rabiu <sulaymancodes@gmail.com>
2024-12-06 09:02:08 +0100

    Project Dependencies Installed

---
+++ app.js
@@ -0,0 +1,6 @@
+const path = require("node:path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
